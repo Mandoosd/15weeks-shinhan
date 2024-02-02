e3f60dedb9b07fd34d0564410c5f4641e78f6e5f
Mandoosd <148739244+Mandoosd@users.noreply.github.com>
2024-02-02 16:29:32 +0900

    Add files via upload

--- js/custom.js
+++ js/custom.js
@@ -3,24 +3,44 @@ $(function () {
 
     const SECTION = document.querySelectorAll('.section');
 
-    const Fullpage = new fullpage('.content', {
+    const Fullpage = new fullpage('#content', {
         scrollOverflow: false, // line-height : 1 에서 스크롤 생기는 거 없애줘...
         paddingTop: '1px',
-        afterLoad: function (o, d, t) {
+        responsiveWidth: 1201,
+        onLeave: function (o, d, t) {
             console.log(d.index);
             SECTION.forEach(it => it.classList.remove('on'));
             SECTION[d.index].classList.add('on');
+
+            if (d.index == 0) {
+                document.querySelector('.header').classList.remove('on')
+            } else {
+                document.querySelector('.header').classList.add('on')
+            }
         }
+
+
+
+
     });
+
+
 
 
     const MAIN_VISUAL_SLIDE = new Swiper('.main_visual_slide', {
         loop: true,
+        pagination: {
+            el: ".swiper-pagination",
+            clickable: true,
+            renderBullet: function (index, className) {
+                return '<span class="' + className + '">' + (index + 1) + "</span>";
+            },
+        },
         autoplay: {
             delay: 4000,
         },
         effect: 'fade',
-        speed: 2000,
+        speed: 1000,
         fadeEffect: {
             crossFade: true
         },
@@ -31,10 +51,12 @@ $(function () {
         autoplay: {
             delay: 4000,
         },
-        scrollbar: {
-            el: ".swiper-scrollbar",
-            hide: true,
+        pagination: {
+            el: ".swiper-pagination",
+            type: "progressbar",
         },
+
+
 
     });
 
@@ -43,17 +65,32 @@ $(function () {
         autoplay: {
             delay: 4000,
         },
-        scrollbar: {
-            el: ".swiper-scrollbar",
-            hide: true,
+        navigation: {
+            nextEl: ".swiper-button-next",
+            prevEl: ".swiper-button-prev",
         },
 
     });
 
     const PREMIUM_SLIDE = new Swiper('.premium_slide', {
         loop: true,
-        slidesPerView: 4,
-        spaceBetween: 20,
+
+        breakpoints: {
+            500: {
+                slidesPerView: 1,
+                spaceBetween: 40,
+            },
+
+            768: {
+                slidesPerView: 3,
+                spaceBetween: 40,
+            },
+
+            1200: {
+                slidesPerView: 4,
+                spaceBetween: 50,
+            },
+        },
 
         scrollbar: {
             el: ".swiper-scrollbar",
